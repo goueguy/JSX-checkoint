3798a625cef1a0606c75a2ba33ac2797098f3b48
alexis225 <jean-louis.goueguy@groupensia.com>
2022-06-07 11:00:39 +0000

    show video on Image component

--- src/Image.js
+++ src/Image.js
@@ -5,15 +5,13 @@ function Image(){
     return (
         <div>
             <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
-            <h1 class="title red">Your name here</h1>
+            <br />
+
+            <img src={flag} alt="flag"/>
 
             <br />
 
-            <img src={flag} alt="cicture"/>
-
-            <br />
-
-            <img src={employeePicture} alt="cicture"/>
+            <img src={employeePicture} alt="flag"/>
 
           </div>
         <iframe width="500" height="500" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
